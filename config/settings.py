bb58c76fb8ace3426b8372b56e7cc0b6c7172eed
Mrst1tch <nturekul@gmail.com>
2020-09-10 17:59:27 +0600

    :bug: removed module

--- config/settings.py
+++ config/settings.py
@@ -2,8 +2,9 @@ import environ
 
 import os
 
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 env = environ.Env()
 environ.Env.read_env(env_file=os.path.join(BASE_DIR, '.env'))
