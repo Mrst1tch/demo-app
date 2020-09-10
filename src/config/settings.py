751c4f5aec63ef7681129b96e2a7c9c955049ab5
Mrst1tch <nturekul@gmail.com>
2020-09-10 17:34:58 +0600

    Add commit message

--- src/config/settings.py
+++ src/config/settings.py
@@ -12,7 +12,7 @@ environ.Env.read_env(env_file=os.path.join(BASE_DIR, '.env'))
 SECRET_KEY = env.str('SECRET_KEY') 
 DEBUG = env.bool('DEBUG') 
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [rbk-demo-app.herokuapp.com]
 
 
 
