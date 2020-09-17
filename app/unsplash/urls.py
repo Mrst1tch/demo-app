daec3ca25541d66ef7095902464c721f3c61387c
Mrst1tch <nturekul@gmail.com>
2020-09-17 17:07:47 +0600

    .

--- app/unsplash/urls.py
+++ app/unsplash/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import  detail_view
+from .views import index_view, search_view, detail_view
 
 urlpatterns = [
-    path('', detail_view, name='detail'),
+    path('search/', search_view, name='search'),
+    path('admin/', detail_view, name='detail'),
+    path('detail/<str:id>/', detail_view, name='detail'),
+
 
 ]
