affceaf8d7c570c7e3f5396efdb4d779d33b53e1
Mrst1tch <nturekul@gmail.com>
2020-09-17 17:00:56 +0600

    .

--- app/unsplash/urls.py
+++ app/unsplash/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from .views import  detail_view
 
 urlpatterns = [
-    path('', detail_view, name='detail'),
+    path('detail/<str:id>/', detail_view, name='detail'),
+
 ]
--- app/unsplash/views.py
+++ app/unsplash/views.py
@@ -1,6 +1,7 @@
 import requests
-from django.shortcuts import render
 from django.conf import settings
+from django.shortcuts import render
+
 
 # Create your views here.
 
@@ -10,21 +11,28 @@ def getphoto():
     url = f'{BASE_URL}/photos/random'
     params = {
         'count':'10',
-        'client_id':settings.ACCESS_KEY
+        'client_id':settings.UNSPLASH_ACCESS_KEY
     }
-    request_param = requests.get(url, params=params)
-    request_param = request_param.json()
+    response = requests.get(url, params=params)
+    response = response.json()
 
-    return request_param
+    return response
 
 def detail_photo_url(id_photo):
     url = f'{BASE_URL}/photos/{id_photo}'
     params = {
-        'client_id':settings.ACCESS_KEY
+        'client_id':settings.UNSPLASH_ACCESS_KEY
     }
-    response_param = requests.get(url, params=params)
-    response_param = response_param.json()
-    return response_param
+    response = requests.get(url, params=params)
+    response = response.json()
+    return response
+
+def detail_view(request, id):
+    response = detail_photo_url(id)
+    context = {
+        'response': response
+    }
+    return render(request, 'detail.html', context)
 
 
     
